4c1faf0ad594431b32e9971a3f25df74dcbb12de
Henry0005 <103142324+Henry0005@users.noreply.github.com>
2024-08-08 21:51:15 -0400

    Uploading my Shell Scripts

---
+++ Primeiro Checkpoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir Henrique
+mkdir Henrique/defesa_cibernetica
+
+mkdir Henrique/defesa_cibernetica/hacker_mindset
+mkdir Henrique/defesa_cibernetica/linux
+mkdir Henrique/defesa_cibernetica/redes
+
+touch Henrique/defesa_cibernetica/hacker_mindset/notas.txt
+touch Henrique/defesa_cibernetica/linux/notas.txt
+touch Henrique/defesa_cibernetica/redes/notas.txt
+
+echo "Sem notas cadastradas" > Henrique/defesa_cibernetica/hacker_mindset/notas.txt
+echo "Sem notas cadastradas" > Henrique/defesa_cibernetica/linux/notas.txt
+echo "Sem notas cadastradas" > Henrique/defesa_cibernetica/redes/notas.txt
